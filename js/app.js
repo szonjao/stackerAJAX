046b1747a2fac9e62111bb800fb7eff0582ef3f1
szonjao <szonja.o@me.com>
2016-01-02 22:01:58 +0100

    initial

--- js/app.js
+++ js/app.js
@@ -31,12 +31,38 @@ var showQuestion = function(question) {
 	return result;
 };
 
+var showTopQuestion = function(item) {
+	// clone our result template code
+	var topresult = $('.templates .top').clone();
+	
+	// Set the username in result
+	var topElem = topresult.find('.user-name a');
+	topElem.attr('href', top.link);
+	topElem.text(item.user.display_name);
+
+	// set the post count property in result
+	var postCount = topresult.find('.post-count');
+	postCount.text(item.post_count);
+
+	// set the score in result
+	var score = topresult.find('.score');
+	score.text(item.score);
+
+	return topresult;
+};
 
 // this function takes the results object from StackOverflow
 // and returns the number of results and tags to be appended to DOM
 var showSearchResults = function(query, resultNum) {
 	var results = resultNum + ' results for <strong>' + query + '</strong>';
 	return results;
+};
+
+// this function takes the results object from StackOverflow
+// and returns the number of results and tags to be appended to DOM
+var showTopSearchResults = function(query, resultNum) {
+	var topresults = resultNum + ' results for <strong>' + query + '</strong>';
+	return topresults;
 };
 
 // takes error string and turns it into displayable DOM element
@@ -81,6 +107,46 @@ var getUnanswered = function(tags) {
 	});
 };
 
+// takes a string of semi-colon separated tags to be searched
+// for on StackOverflow for To Answerers
+var getTopAnswerers = function(tags) {
+	console.log("----");
+	console.log(tags);
+	
+	// the parameters we need to pass in our request to StackOverflow's API
+	var requestTop = { 
+		tagged: tags,
+		site: 'stackoverflow',
+		order: 'desc',
+		sort: 'creation'
+	};
+	
+	$.ajax({
+		url: "http://api.stackexchange.com/2.2/tags/html/top-answerers/all_time",
+		data: requestTop,
+		dataType: "jsonp",//use jsonp to avoid cross origin issues
+		type: "GET",
+	})
+	.done(function(topresult){ //this waits for the ajax to return with a succesful promise object
+		console.log("++++");
+		console.log(requestTop);
+
+		var searchTopResults = showSearchResults(requestTop.tagged, topresult.items.length);
+
+		$('.search-results').html(searchTopResults);
+		//$.each is a higher order function. It takes an array and a function as an argument.
+		//The function is executed once for each item in the array.
+		$.each(topresult.items, function(i, item) {
+			var top = showTopQuestion(item);
+			$('.results').append(top);
+		});
+	})
+	.fail(function(jqXHR, error){ //this waits for the ajax to return with an error promise object
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
+	});
+};
+
 
 $(document).ready( function() {
 	$('.unanswered-getter').submit( function(e){
@@ -90,5 +156,13 @@ $(document).ready( function() {
 		// get the value of the tags the user submitted
 		var tags = $(this).find("input[name='tags']").val();
 		getUnanswered(tags);
+	});
+	$('.inspiration-getter').submit( function(e){
+		e.preventDefault();
+		// zero out results if previous search has run
+		$('.results').html('');
+		// get the value of the tags the user submitted
+		var tags = $(this).find("input[name='answerers']").val();
+		getTopAnswerers(tags);
 	});
 });
